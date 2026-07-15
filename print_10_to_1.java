0d00f5568ac6097518d8d14e67750118c3ce445b
andy899898 <desimunda67467@gmail.com>
2026-07-15 17:25:07 +0530

    Delete print_10_to_1.java

--- print_10_to_1.java
+++
@@ -1,9 +0,0 @@
-public class print_10_to_1 {
-    public static void main(String[] args) {
-        int i = 10;
-        while(i>=1){
-            System.out.println(i);
-            i--;
-        }
-    }
-}
